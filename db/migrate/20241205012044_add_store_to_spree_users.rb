408dbf4fa514adf9f096bd3d1e835251e0f2beca
Arthur Macedo <arthurmacedo01@gmail.com>
2024-12-05 11:43:13 -0300

    adjust

--- db/migrate/20241205012044_add_store_to_spree_users.rb
+++ db/migrate/20241205012044_add_store_to_spree_users.rb
@@ -1,15 +1,5 @@
 class AddStoreToSpreeUsers < ActiveRecord::Migration[6.0]
   def change
-    unless column_exists?(:spree_users, :store_id)
-      add_reference :spree_users, :store, null: false, foreign_key: true
-    end
-
-    # Set default store for existing users
-    reversible do |dir|
-      dir.up do
-        default_store = Spree::Store.first
-        Spree::User.update_all(store_id: default_store.id) if default_store.present?
-      end
-    end
+    add_reference :spree_users, :store, foreign_key: { to_table: :spree_stores }
   end
 end
