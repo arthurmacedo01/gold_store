e0ec264905925b229d4df42844c246c1a7e65f4d
Arthur Macedo <arthurmacedo01@gmail.com>
2024-12-07 23:45:19 -0300

    blank

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,7 @@ class HomeController < StoreController
     @products = @searcher.retrieve_products
     @products = Spree::Product.available.where(store_id: current_store.id)
 
+
     # Split products into groups of 3 for the homepage blocks.
     # You probably want to remove this logic and use your own!
     homepage_groups = @products.in_groups_of(3, false)
